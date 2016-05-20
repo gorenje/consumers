1e0e9131a3e1602039e66f2d64d1aa86458bc053
Gerrit Riessen <gerrit.riessen@gmail.com>
2016-05-20 14:57:54 +0200

    no haml included in project
    On branch: master

--- lib/auth_filter.rb
+++ lib/auth_filter.rb
@@ -5,10 +5,18 @@ class AuthFilter
 
   def call(env)
     request = Rack::Request.new(env)
-    unless request.session[:authenticated] || request.path_info =~ /^\/(auth|pingdom|api)/
-      request.session[:lgkp] = request.path
-      return [ 307, { 'Location' => '/auth/google_oauth2'}, []]
+
+    # server public stuff
+    if request.path_info =~ /^\/(js|css|img|images|fonts)/
+      return @app.call(env)
     end
+
+    unless request.session[:access_token]
+      unless (['/accessdenied', '/logout','/error','/auth','/auth/google_oauth2','/oauth2callback'].include?(request.path_info) || request.path_info =~ /^\/auth/)
+        return [ 307, { 'Location' => '/auth'}, []]
+      end
+    end
+
     return @app.call(env)
   end
 end
--- routes/authentication.rb
+++ routes/authentication.rb
@@ -1,29 +1,53 @@
-get '/auth' do
-  redirect '/auth/google_oauth2'
+# Scopes are space separated strings
+SCOPES = [
+    'https://www.googleapis.com/auth/userinfo.email'
+].join(' ')
+
+get "/auth" do
+  redirect client.auth_code.authorize_url(:redirect_uri => redirect_uri,:scope => SCOPES,:access_type => "offline")
 end
 
-get '/auth/:provider/callback' do
-  @email = request.env['omniauth.auth']["info"]["email"]
+get '/oauth2callback' do
+  access_token = client.auth_code.get_token(params[:code], :redirect_uri => redirect_uri)
+
+  # parsed is a handy method on an OAuth2::Response object that will
+  # intelligently try and parse the response.body
+  @email = access_token.
+    get('https://www.googleapis.com/userinfo/email?alt=json').
+    parsed["data"]["email"]
+
   if ENV['ACCESS_DOMAINS'].split(/,/).any? { |a| @email =~ /@#{a}$/ } ||
       (ENV["ACCESS_EMAILS"] || "").split(",").include?(@email)
-    session[:authenticated] = true
-    session[:user] = {
-      :user => request.env['omniauth.auth']["info"],
-      :uid  => request.env['omniauth.auth']["uid"],
-    }
-    redirect session[:lgkp] || '/'
-  else
-    session[:authenticated] = false
+    session[:access_token] = access_token.token
+    session[:email]        = @email
+
+    @message = "Successfully authenticated with the server"
+    @access_token = session[:access_token]
     redirect '/'
+  else
+    session[:access_token] = nil
+    session[:unknownemail] = @email
+    redirect '/accessdenied'
   end
 end
 
-get '/auth/failure' do
-  session[:authenticated] = false
-  erb "<h1>Authentication Failed:</h1><h3>message:<h3> <pre>#{params}</pre>"
+get '/accessdenied' do
+  @email = session[:unknownemail]
+  "No Access Allowed for #{@email}"
 end
 
-get '/auth/logout' do
-  session[:authenticated] = false
-  redirect '/'
+def redirect_uri
+  uri = URI.parse(request.url)
+  uri.path = '/oauth2callback'
+  uri.query = nil
+  uri.to_s
+end
+
+def client
+  client ||=
+    OAuth2::Client.new(ENV['GOOGLE_CLIENT_ID'],
+                       ENV['GOOGLE_CLIENT_SECRET'], {
+                         :site          => 'https://accounts.google.com',
+                         :authorize_url => "/o/oauth2/auth",
+                         :token_url     => "/o/oauth2/token"})
 end
