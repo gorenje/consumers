786d61ff6ed31c834b88e3b42fc52fa50369a31d
Gerrit Riessen <gerrit.riessen@gmail.com>
2016-05-23 15:37:59 +0200

    removed unnecessary code
    On branch: master

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -20,25 +20,11 @@ cron_jobs = [
              },
 ]
 
-module EccrineAnalytics
-  class ActiveRecordLocalMiddleware
-    def call(worker, job, queue)
-      yield
-    ensure
-      ActiveRecord::Base.clear_active_connections! if defined?(::ActiveRecord)
-    end
-  end
-end
-
 Sidekiq.configure_server do |config|
   config.redis = { :url => ENV['REDISTOGO_URL'], :driver => :hiredis,
     :size => (ENV["REDIS_POOL_SIZE"] || 5).to_i }
 
   Sidekiq::Cron::Job.load_from_array cron_jobs
-
-  # config.server_middleware do |chain|
-  #   chain.add EccrineAnalytics::ActiveRecordLocalMiddleware
-  # end
 end
 
 Sidekiq.configure_client do |config|
