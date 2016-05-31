bae657439afb56cf71fb7e7ef3f159f25f651990
Gerrit Riessen <gerrit.riessen@gmail.com>
2016-05-31 18:26:10 +0200

    do not remove the 'e'
    On branch: master

--- lib/schedulers/url_tracking.rb
+++ lib/schedulers/url_tracking.rb
@@ -9,7 +9,7 @@ module Scheduler
     def initialize(batch_size = 200, job_life_time = 58, sleep_between_execution = 1)
       @redis_queue           = RedisQueue.new($redis.local, :tracking_url_queue)
       @batch_size            = batch_size
-      @job_life_tim          = job_life_time
+      @job_life_time         = job_life_time
       @sleep_between_execution = sleep_between_execution
     end
 
