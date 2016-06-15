dbaeac5a80fc9ef71c0ec5f3a6df26d8f625075a
Gerrit Riessen <gerrit.riessen@gmail.com>
2016-06-15 14:58:44 +0200

    uses kafka batches, not single messages
    On branch: master

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -9,7 +9,8 @@ $redis = OpenStruct.new.tap do |os|
     'click_store' => 'CLICK_REDIS_URL'
   }.each do |name, env_name|
     os[name] = ConnectionPool.
-      new(:size => (ENV['REDIS_POOL_SIZE'] || '5').to_i) do
+      new(:size => (ENV["REDIS_POOL_SIZE_#{env_name}"] ||
+                    ENV['REDIS_POOL_SIZE'] || '5').to_i) do
       Redis.new(:url => ENV[env_name], :driver => :hiredis)
     end
   end
