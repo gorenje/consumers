07da496b485f1b95c9604ca6678de4fa4fef705c
Gerrit Riessen <gerrit.riessen@gmail.com>
2016-06-17 16:39:27 +0200

    update librato and ignore failures - prevent librato from bring down the entire show
    On branch: master

--- config/initializers/librato.rb
+++ config/initializers/librato.rb
@@ -5,8 +5,10 @@ Librato::Metrics.authenticate(ENV['LIBRATO_USER'], ENV['LIBRATO_TOKEN'])
 
 $librato_queue      ||= Librato::Metrics::Queue.new(
   :prefix              => (ENV['LIBRATO_PREFIX']||"consumers"),
-  :autosubmit_interval => 60)
+  :autosubmit_interval => 60,
+  :clear_failures      => true)
 
 $librato_aggregator ||= Librato::Metrics::Aggregator.new(
   :prefix              => (ENV['LIBRATO_PREFIX']||"consumers"),
-  :autosubmit_interval => 120)
+  :autosubmit_interval => 120,
+  :clear_failures      => true)
