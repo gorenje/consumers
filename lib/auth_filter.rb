27d7c5dd1619717fa1a4305111f61381c4a42e4e
Gerrit Riessen <gerrit.riessen@gmail.com>
2017-02-21 16:08:42 +0100

    forgot to add lets encrypt path to auth filter exception
    On branch: master

--- lib/auth_filter.rb
+++ lib/auth_filter.rb
@@ -14,7 +14,8 @@ class AuthFilter
     unless request.session[:authenticated]
       unless (['/accessdenied', '/auth', '/oauth2callback', '/api/1/create',
                '/pingdom', '/api/1/delete'].
-              include?(request.path_info) || request.path_info =~ /^\/auth/)
+              include?(request.path_info) || request.path_info =~ /^\/auth/ ||
+              request.path_info =~ /\/\.well-known\/acme-challenge\//)
         url = request.scheme + "://" + request.host_with_port + "/auth"
         return [ 307, { 'Location' => url }, []]
       end
